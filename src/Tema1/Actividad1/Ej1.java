05cf0f969b2400a668b63e5914ff3a602e547e64
jojoseja <jfeyjoo@gmail.com>
2025-10-03 11:50:28 +0200

    fixed ejercicio1

---
+++ src/Tema1/Actividad1/Ej1.java
@@ -0,0 +1,28 @@
+package Tema1.Actividad1;
+
+public class Ej1 {
+    public static void main(String[] args) {
+        try {
+            Process p = Runtime.getRuntime().exec("java -cp out/production/PSP Tema1.Actividad1.LeerNombre hola");
+
+            /*
+            InputStreamReader isr = new InputStreamReader(p.getInputStream());
+
+            BufferedReader br = new BufferedReader(isr);
+
+            String linea;
+            while ((linea = br.readLine()) != null) {
+                System.out.println(linea);
+            }
+
+            br.close();
+            */
+            System.out.println(p.waitFor());
+
+            System.out.println(p.exitValue());
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
+
